435f56aac943f96d1f4b3f51b34e4c2d7b3d1b12
g1gasad <asadahsan2245@gmail.com>
2025-04-05 14:10:49 +0530

    added sig(signal) prefix to control unit output signals

--- srcs/ALU.v
+++ srcs/ALU.v
@@ -1,26 +1,25 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/02/2025 08:59:52 PM
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module ALU(a, b, sig_alu_control, result, sig_zero);
+input [15:0] a, b;
+input [2:0] sig_alu_control;
+output reg [15:0] result;
+output reg sig_zero;
 
+always@(*) begin
+case(sig_alu_control)
+	3'b000: result=a+b;    // add result, a, b
+	3'b001: result=a-b;    // sub result, a, b
+	3'b010: result=a&b;    // and result, a, b
+	3'b011: result=a|b;    // or result, a, b
+	3'b100: // this is for slt: set on less than
+		if (a<b) result=16'd1; // if a<b, result=1
+		else result=16'd0;     // else, result=1
+	default: result=a+b;
+endcase
+end
 
-module ALU(
-
-    );
+always@(result) begin // we have added this "sig_zero" param just
+if (result==16'b0)    // to check when we get 0 as result
+	sig_zero=1'b1;		  // if 0, zero will be 1, denoting 0's presence
+else
+	sig_zero=1'b0; end
 endmodule
--- srcs/ALUcontrol.v
+++ srcs/ALUcontrol.v
@@ -1,22 +1,22 @@
-module ALUcontrol(sig_ALUop, func, alu_control);
+module ALUcontrol(sig_ALUop, func, sig_alu_control);
 input [1:0] sig_ALUop;
 input [3:0] func;
-output reg [2:0] alu_control;
+output reg [2:0] sig_alu_control;
 wire [5:0] ALUcontrolfn;
 
 assign ALUcontrolfn = {sig_ALUop, func};
 
 always@(ALUcontrolfn) begin
 casex(ALUcontrolfn)
-6'b11xxxx: alu_control=3'b000;
-6'b10xxxx: alu_control=3'b001;
-6'b01xxxx: alu_control=3'b010;
-6'b000000: alu_control=3'b011;
-6'b000001: alu_control=3'b000;
-6'b000010: alu_control=3'b001;
-6'b000011: alu_control=3'b010;
-6'b000100: alu_control=3'b100;
-default: alu_control=3'b000;
+6'b11xxxx: sig_alu_control=3'b000;
+6'b10xxxx: sig_alu_control=3'b001;
+6'b01xxxx: sig_alu_control=3'b010;
+6'b000000: sig_alu_control=3'b011;
+6'b000001: sig_alu_control=3'b000;
+6'b000010: sig_alu_control=3'b001;
+6'b000011: sig_alu_control=3'b010;
+6'b000100: sig_alu_control=3'b100;
+default: sig_alu_control=3'b000;
 endcase
 end
 
