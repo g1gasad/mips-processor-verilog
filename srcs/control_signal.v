08c303044fabbac335f9fe6872e7d379427f160a
Asad Ahsan <asadahsan2245@gmail.com>
2025-04-19 11:13:26 +0530

    Update control_signal.v - indentation changes

--- srcs/control_signal.v
+++ srcs/control_signal.v
@@ -7,8 +7,8 @@ output reg sig_regDst, sig_jump, sig_branch, sig_memRead, sig_memtoReg, sig_memW
 		   sig_ALUsrc, sig_regWrite, sign_or_zero;
 always@(*) begin
 if (rst==1'b1) begin
-	sig_ALUop <= 2'b00;
-	sig_regDst <= 1'b0;
+    sig_ALUop <= 2'b00;
+    sig_regDst <= 1'b0;
     sig_jump <= 1'b0;
     sig_branch <= 1'b0;
     sig_memRead <= 1'b0;
@@ -20,13 +20,13 @@ if (rst==1'b1) begin
 else begin
 case(opcode)
 3'b000: begin // add, sub, and, or
-		sig_regDst <= 1'b1;
-		sig_jump <= 1'b0;
-		sig_branch <= 1'b0;
+	sig_regDst <= 1'b1;
+	sig_jump <= 1'b0;
+	sig_branch <= 1'b0;
         sig_memRead <= 1'b0;
         sig_memtoReg <= 1'b0;
         sig_ALUop <= 2'b00;
-		sig_memWrite <= 1'b0;
+	sig_memWrite <= 1'b0;
         sig_ALUsrc <= 1'b0;
         sig_regWrite <= 1'b1;
         
